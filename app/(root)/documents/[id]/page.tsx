0403a9682bc13c7ddd0a62198cacccbb86a79073
{suharshit} <{suharshit123@gmail.com}>
2024-07-31 18:16:21 +0530

    new changes added

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,24 +1,25 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { getDocument } from '@/lib/actions/room.actions';
+import { currentUser, EmailAddress } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Document = () => {
+const Document = async({ params: { id }}: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress
+  })
+  if(!room) redirect('/')
+  // TODO: Assess the permissions of the user to access the document
   return (
-    <div>
-      <Header>
-        <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='doucment-title'>Share</p>
-        </div>
-        <SignedOut>
-          <SignInButton/>
-        </SignedOut>
-        <SignedIn>
-          <UserButton/>
-        </SignedIn>
-      </Header>
-      <Editor />
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata}
+      />
+    </main>
   )
 }
 
